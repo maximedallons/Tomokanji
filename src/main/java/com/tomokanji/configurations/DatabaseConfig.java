08de95e0ce32ecfd2140763762212a1c553e3de8
Maxime <maxime@quixiz.com>
2024-11-30 01:20:45 +0900

    Externalized db connection informations

--- src/main/java/com/tomokanji/configurations/DatabaseConfig.java
+++ src/main/java/com/tomokanji/configurations/DatabaseConfig.java
@@ -1,22 +1,38 @@
 package com.tomokanji.configurations;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
+import javax.annotation.PostConstruct;
 import javax.sql.DataSource;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Properties;
 
 @Configuration
 public class DatabaseConfig {
 
+    @Value("${dbPropertiesPath}")
+    private String dbPropertiesPath;
+
+    private Properties dbProperties = new Properties();
+
+    @PostConstruct
+    private void loadDbProperties() throws IOException {
+        dbProperties.load(Files.newBufferedReader(Paths.get(dbPropertiesPath)));
+    }
+
     @Bean
     public DataSource dataSource() {
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
-        dataSource.setUrl("jdbc:mysql://89.116.51.243:3306/tomokanji");
-        dataSource.setUsername("gami");
-        dataSource.setPassword("M@xime24061999");
+        dataSource.setDriverClassName(dbProperties.getProperty("datasource.driver-class-name"));
+        dataSource.setUrl(dbProperties.getProperty("datasource.url"));
+        dataSource.setUsername(dbProperties.getProperty("datasource.username"));
+        dataSource.setPassword(dbProperties.getProperty("datasource.password"));
         return dataSource;
     }
 
